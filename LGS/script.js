c0bebb979fcd1580307cfd706974f58c514d67af
Jan <jan.stoerk@sap.com>
2024-04-08 10:45:54 +0200

    funktionsweise angepasst

--- LGS/script.js
+++ LGS/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const clearButton = document.getElementById('clearButton');
     const addButton = document.getElementById('addButton');
     const removeButton = document.getElementById('removeButton');
+    const addButtonColumn = document.getElementById('addButtonColumn');
+    const removeButtonColumn = document.getElementById('removeButtonColumn');
 
     function createInputFields() {
         const row = table.insertRow();
@@ -12,20 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
             const input = document.createElement('input');
             input.type = 'text';
             cell.appendChild(input);
-            if (i < 3) {
+            /*if (i < 3) {
                 const plusSign = document.createTextNode('+');
                 cell.appendChild(plusSign);
             }
             else if (i != 4) {
                 const timesSign = document.createTextNode('=');
                 cell.appendChild(timesSign);
-            }
+            }*/
         }
     }
 
     for (let i = 0; i < 4; i++) {
         createInputFields();
     }
+    addSigns();
 
     saveButton.addEventListener('click', function () {
         const inputs = document.querySelectorAll('#Gleichungssystem input');
@@ -49,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(cells)
         if (rows > 1) {
             table.deleteRow(rows - 1);
+            addSigns();
         }
     });
 
@@ -62,15 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 const input = document.createElement('input');
                 input.type = 'text';
                 cell.appendChild(input);
-                if (i < 3) {
-                    const plusSign = document.createTextNode('+');
-                    cell.appendChild(plusSign);
-                }
-                else if (i != 4) {
-                    const timesSign = document.createTextNode('=');
-                    cell.appendChild(timesSign);
-                }
             }
+            addSigns();
         }
     });
 
@@ -81,6 +78,50 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let i = 0; i < 4; i++) {
             createInputFields();
         }
+        addSigns();
     });
+
+    removeButtonColumn.addEventListener('click', function () {
+        if(table.rows[0].cells.length > 2){
+            for (var i = 0; i < table.rows.length; i++) {
+                cellNumber = table.rows[i].cells.length;
+                table.rows[i].deleteCell(cellNumber - 1);
+            }
+        }
+        addSigns();
+    });
+
+    addButtonColumn.addEventListener('click', function () {
+        if(table.rows[0].cells.length < 10){
+            for (var i = 0; i < table.rows.length; i++) {
+                const cell = table.rows[i].insertCell();
+                const input = document.createElement('input');
+                input.type = 'text';
+                cell.appendChild(input);
+            }
+            addSigns();
+        }
+    });
+
+    function addSigns(){
+        for (var i = 0; i < table.rows.length; i++) {
+            var columnLength = table.rows[i].cells.length;
+            for(var j =0;j<columnLength;j++){
+                var cell = table.rows[i].cells[j];
+                if (cell.childNodes.length > 1) {
+                    cell.removeChild(cell.childNodes[1]);
+                }
+                if(j+2 == columnLength){
+                    const equalsSign = document.createTextNode('=');
+                    cell.appendChild(equalsSign);
+                }else if(j+1 !=columnLength){
+                    const plusSign = document.createTextNode('+');
+                    cell.appendChild(plusSign); 
+                }
+
+            }
+
+        }
+    }
 
 });
