15b9b4bcbdce197aa2bb3c25ae2880fd8084339b
Jan <jan.stoerk@sap.com>
2024-07-07 18:32:12 +0200

    LGS App

--- LandingPage/animations.js
+++ LandingPage/animations.js
@@ -23,3 +23,5 @@ function addHoverAnimation(elementId, images, imageElementId) {
 addHoverAnimation("LGS", ["../pictures/LoesungLinearerGleichungssystemeLogo.png", "../pictures/LoesungLinearerGleichungssystemeLogo2.png", "../pictures/LoesungLinearerGleichungssystemeLogo3.png", "../pictures/LoesungLinearerGleichungssystemeLogo4.png", "../pictures/LoesungLinearerGleichungssystemeLogo5.png", "../pictures/LoesungLinearerGleichungssystemeLogo6.png"], "lgsImage");
 //Animation für Nicht lineare Ausgliechsprobleme
 addHoverAnimation("NLA", ["../pictures/NichtLineareAusgleichsproblemeLogo.png", "../pictures/NichtLineareAusgleichsproblemeLogo2.png", "../pictures/NichtLineareAusgleichsproblemeLogo3.png", "../pictures/NichtLineareAusgleichsproblemeLogo4.png", "../pictures/NichtLineareAusgleichsproblemeLogo5.png", "../pictures/NichtLineareAusgleichsproblemeLogo6.png"], "nlaImage");
+//Animation für Numerische Integration
+addHoverAnimation("Integration", ["../pictures/IntegrationLogo0.png", "../pictures/IntegrationLogo1.png", "../pictures/IntegrationLogo2.png", "../pictures/IntegrationLogo3.png", "../pictures/IntegrationLogo4.png"], "integrationImage");
